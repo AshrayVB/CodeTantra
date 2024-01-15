29428968168f7cce397bbc3cb39300ff5de38f91
Ashray <78349565+AshrayVB@users.noreply.github.com>
2024-01-15 11:26:07 +0530

    Update CapitalWordsCounter.java

--- CodeTantra Java/src/Module1/CapitalWordsCounter.java
+++ CodeTantra Java/src/Module1/CapitalWordsCounter.java
@@ -1,3 +1,11 @@
+// Write a program to count the number of words that start with capital letters.
+// Test case 1
+// 	Enter·a·line:·Hello there Are Some Capital And Some small words	
+// 	Capital·letters:·6	
+// Test case 2
+// 	Enter·a·line:·The Train Leaves Early Morning at 8 AM	
+// 	Capital·letters:·7
+
 package Module1;
 import java.util.Scanner;
 
