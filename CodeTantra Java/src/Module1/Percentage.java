68d1a78a669d528611b12a9c50d00b2c138a6ab1
Ashray <78349565+AshrayVB@users.noreply.github.com>
2024-01-16 21:43:50 +0530

    Update Percentage.java

--- CodeTantra Java/src/Module1/Percentage.java
+++ CodeTantra Java/src/Module1/Percentage.java
@@ -1,3 +1,22 @@
+// Write a program that calculates the percentage marks of the student if marks in 6 subjects are given.
+// Sample Test Cases
+	// Test case 1
+	//	 Enter·the·marks·for·subject1:·56	
+		// Enter·the·marks·for·subject2:·87	
+		// Enter·the·marks·for·subject3:·29	
+		// Enter·the·marks·for·subject4:·55	
+		// Enter·the·marks·for·subject5:·36	
+		// Enter·the·marks·for·subject6:·48	
+		// Percentage:·51.0
+	// Test case 2
+		// Enter·the·marks·for·subject1:·85	
+		// Enter·the·marks·for·subject2:·66	
+		// Enter·the·marks·for·subject3:·97	
+		// Enter·the·marks·for·subject4:·85	
+		// Enter·the·marks·for·subject5:·69	
+		// Enter·the·marks·for·subject6:·98	
+		// Percentage:·83.0
+
 package Module1;
 import java.util.Scanner;
 
