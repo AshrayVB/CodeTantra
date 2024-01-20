7af57a78d5e7318e7abed54c8f536156437c3cf5
Ashray <78349565+AshrayVB@users.noreply.github.com>
2024-01-20 18:17:24 +0530

    Update SubString.java

--- CodeTantra Java/src/Module1/SubString.java
+++ CodeTantra Java/src/Module1/SubString.java
@@ -1,3 +1,13 @@
+// Write a program to find the length of the string and print the second half of the string.
+//     Test case 1
+//         Enter·a·string:·Programming	
+//         Length·of·the·string·Programming·is:·11⏎	
+//         Sub·string·is:·amming
+//     Test case 2
+//         Enter·a·string:·Thankyou	
+//         Length·of·the·string·Thankyou·is:·8⏎	
+//         Sub·string·is:·kyou
+
 package Module1;
 import java.util.Scanner;
 
