d64643ca89cefbbc418853909748ecb662b0663c
Ashray <78349565+AshrayVB@users.noreply.github.com>
2024-02-02 23:17:01 +0530

    Add files via upload

---
+++ CodeTantra Java/Module-1/src/Module1/BitwiseXOR.java
@@ -0,0 +1,13 @@
+//Write a program for XOR operation
+package Module1;
+
+import java.util.*;
+
+public class BitwiseXOR{
+	public static void main(String[] str){
+		int a=Integer.parseInt(str[0]);
+		int b=Integer.parseInt(str[1]);
+		System.out.println("XOR Result = "+(a^b));
+		
+	}
+}
